46775c44e487dff336d8628d8ad68f4dbd482070
Rashindrie <rashindrie.14@cse.mrt.ac.lk>
2018-06-17 13:59:45 +0530

    scraped data

--- swiftSearch/spiders/yamu_spider.py
+++ swiftSearch/spiders/yamu_spider.py
@@ -26,8 +26,6 @@ class YamuSpider(scrapy.Spider):
     def parse_restaurant(self, response):
         restaurant = ItemLoader(item=YamuItem(), response=response)
         restaurant.add_xpath(field_name='name', xpath='//div[@class="place-title-box"]/h2/text()')
-        restaurant.add_xpath(field_name='contact', xpath='//a[@class="emph"]/text()')
-        restaurant.add_xpath(field_name='address', xpath='//p[@class="addressLine"]/text()')
         restaurant.add_xpath(field_name='description', xpath='//p[@class="excerpt"]/text()')
         restaurant.add_xpath(field_name='cuisine', xpath='//a[contains(@href,"/cuisine/")]/text()')
         restaurant.add_xpath(field_name='price_range', xpath='//a[contains(text(),"Rs.")]/text()')
@@ -40,5 +38,7 @@ class YamuSpider(scrapy.Spider):
                              value=response.xpath('//a[contains(@href,"/rating/service")]/@href').extract_first().split('-')[-1] if response.xpath('//a[contains(@href,"/rating/service")]/@href').extract_first() is not None else 'N/A')
         restaurant.add_value(field_name='ambiance_rating',
                              value=response.xpath('//a[contains(@href,"/rating/ambiance")]/@href').extract_first().split('-')[-1] if response.xpath('//a[contains(@href,"/rating/ambiance")]/@href').extract_first() is not None else 'N/A')
-
+        restaurant.add_xpath(field_name='address', xpath='//p[@class="addressLine"]/text()')
+        restaurant.add_value(field_name='contact',
+                             value=(" ".join((response.xpath('//a[@class="emph"]/text()').extract_first()).split())).split(' ', 1)[1] if response.xpath('//a[@class="emph"]/text()').extract_first() is not None else 'N/A')
         yield restaurant.load_item()
